03f9b8b97f322b89d2a724df83a63f3244dab707
Oscar <martinezr.oscar@gmail.com>
2017-06-04 19:30:19 -0700

    update 6

--- projects/Oscar's capstone/stock_prediction.py
+++ projects/Oscar's capstone/stock_prediction.py
@@ -5,6 +5,7 @@ from sklearn.svm import SVR
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import cross_val_predict, cross_val_score
+from sklearn.dummy import DummyRegressor
 import pandas_datareader.data as web
 import datetime
 import matplotlib.pyplot as plt
@@ -18,43 +19,16 @@ def get_file_name(stock):
     #Method to read file from datasets folder
     return 'datasets/{}.csv'.format(stock)
 
-def save_file(stock):
-	""" This function saves the current dataset"""
-	d = {'one' : pd.Series([1., 2., 3.], index=['a', 'b', 'c']),
-	'two' : pd.Series([1., 2., 3., 4.], index=['a', 'b', 'c', 'd'])}
-	df = pd.DataFrame(d)
-	print df
-
-	df.to_csv(get_file_name(stock))
-
 def read_from_file(stock):
 	#index_col = 'Date'
     dataset = pd.read_csv(get_file_name(stock), parse_dates = True, index_col = 'Date',
     	usecols = ['Date','Open','High','Low','Close','Volume', 'Adj Close'])
     #print dataset.head()
-
     print dataset.corr()
-
     y_all = dataset['Adj Close']
     X_all = dataset.drop(['Adj Close'], axis = 1)
-    #X_all = dataset.drop(['Date'], axis = 1)
-
-
-    #X_all['Trade Date'] = X_all.index
-    #date_ordinal = X_all['Trade Date'].apply(lambda x: x.toordinal())
-    
-    #X_all = X_all.drop(['Trade Date'], axis = 1)
-    #X_all = X_all.join(date_ordinal)
-    #X_all = X_all['Trade Date'].apply(lambda x: x.toordinal())
-    #print date_ordinal.head()
-
     print X_all.head()
     print y_all.head()
-
-    #print X_all.head()
-    # y = pd.read_csv(get_file_name(stock), index_col = 'Date', parse_dates = True, 
-    #	usecols = ['Date','Adj Close'])
-    #print y.head()
     return X_all, y_all
 
 def plot(stock, df, column = 'Adj Close'):
@@ -63,7 +37,7 @@ def plot(stock, df, column = 'Adj Close'):
 	ax.set_xlabel('Date')
 	plt.show()
 
-def plot_results(X_test, X_train, y_test, y_train, model, y):
+def plot_split_results(X_test, X_train, y_test, y_train):
 	stock_date_test = pd.DataFrame()
 	stock_date_train = pd.DataFrame()
 	stock_date_test['Date'] = X_test.index
@@ -73,30 +47,26 @@ def plot_results(X_test, X_train, y_test, y_train, model, y):
 	fig, ax = plt.subplots()
 	ax.plot_date(stock_date_test, y_test, color ='r', label = 'Test data', linewidth=1)
 	ax.plot_date(stock_date_train, y_train, color ='b', label = 'Train data', linewidth=1)
-	#plt.plot(X_test, model.predict(y_test), color = 'b')
-	#ax.set_ylabel('Price')
-	#ax.set_xlabel('Date')
-
-	
-	#print "=======================X_test{}".format(X_test.shape)
-	#print "+++++++++++++++++++++{}".format(y_test.shape)
-	
-
-	#stock_date_test = stock_date_test.sort_values(['Date'])
-	#[y.min(), y.max()], [y.min(), y.max()]
-	#print stock_date_test.head()
-	#ax.plot(stock_date_test, model.predict(X_test), color='black', linewidth=1)
-	#ax.plot(stock_date_test, y_test, 'k--', lw=4)
-	#plt.clf()
-	#plt.scatter(X_test['High'], y_test, color ='r', label = 'Test data', linewidth=1)
-	#plt.scatter(X_train['High'], y_train, color ='b', label = 'Train data', linewidth=1)
-	#plt.plot(X_test['High'], model.predict(X_test), color='black', linewidth=1)
-
-
+	ax.set_xlabel('Date')
+	ax.set_ylabel('Price')
 
 	plt.legend()
-	plt.axis('tight')
-	#plt.title('Model {}'.format(model))
+	plt.title('Splitted dataset')
+	plt.show()
+
+def plot_tunned_model(name, model, X, y):
+	predicted = cross_val_predict(model, X, y, cv=10)
+	scores = cross_val_score(model, X, y, cv=10)
+	print "Scores {}". format(scores)
+	print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+
+	fig, ax = plt.subplots()
+	ax.scatter(y, predicted, label = 'Predicted Price', color ='r')
+	ax.plot([y.min(), y.max()], [y.min(), y.max()], 'k--', lw=4, label = 'Model')
+	ax.set_xlabel('Measured')
+	ax.set_ylabel('Predicted')
+	plt.legend()
+	plt.title('{} trainned model.'.format(name))
 	plt.show()
 
 def main(argv):
@@ -127,114 +97,90 @@ def split_data(X, y):
 	print "********************Splitting the Data******************"
 	print X.shape
 	print y.shape
-	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
 	return X_train, X_test, y_train, y_test
 
-def linear_model(X, y):
+def benchmark(X_train, X_test, y_train, y_test):
+	print "********************DummyRegressor Model******************"
+	model = DummyRegressor()
+	model.fit(X_train, y_train)
+	print '{}'.format(model.score(X_test, y_test))
+	return model
+
+def linear_model(X_train, X_test, y_train, y_test):
 	print "********************LinearRegression Model******************"
-	X_train, X_test, y_train, y_test = split_data(X, y)
 	model = LinearRegression()
 	model.fit(X_train, y_train)
 	print '{}'.format(model.score(X_test, y_test))
-	#print 'Predict {}'.format(model.predict(X_test))
-	#stock_date_test = 
-	#stock_price_test = y_test.drop('Date', 1)
-	print "X_train {}".format(X_train.head())
-	print "y_train {}".format(y_train.head())
-	
-	predicted = cross_val_predict(model, X, y, cv=10)
-	scores = cross_val_score(model, X, y, cv=10)
-	print "Scores {}". format(scores)
-	print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
-
-	fig, ax = plt.subplots()
-	ax.scatter(y, predicted)
-	ax.plot([y.min(), y.max()], [y_test.min(), y.max()], 'k--', lw=4)
-	ax.set_xlabel('Measured')
-	ax.set_ylabel('Predicted')
-	plt.show()
-
-	#print "{}".format(y_test['Adj Close'])
-	#print "{}".format(stock_price_test)
-	#print "{}".format(stock_date_test)
-	#print "{}".format(y_test)
-	plot_results(X_test, X_train, y_test, y_train, model, y)
 	return model
 
+def linear_model_tunned(X_train, X_test, y_train, y_test, X, y):
+	print "********************LinearRegression Tunned Model******************"
+	parameters = {'fit_intercept':[True, False], 'normalize':[True, False]}
+	model = LinearRegression()
+	model = GridSearchCV(model, parameters)
+	model.fit(X_train, y_train)
+	print '{}'.format(model.score(X_test, y_test))
+	plot_tunned_model('LinearRegression', model, X, y)
 
 
-def svr_model(X, y):
-	X_train, X_test, y_train, y_test = split_data(X, y)
+
+def svr_model(X_train, X_test, y_train, y_test, X, y):
+	print "********************Epsilon-Support Vector Regression Model******************"
 	#'kernel':('linear', 'poly', 'rbf', 'sigmoid', 'precomputed')
-	parameters = {'kernel':['rbf'],
-	 'C':[1, 10], 'gamma':[0.1]}
+	#parameters = {'kernel':['rbf'], 'C':[1.0, 10], 'gamma':[0.1, 'auto']}
 
-	#model = GridSearchCV(SVR(kernel='rbf', gamma=0.1), cv=10,
-    #               param_grid={"C": [1e0, 1e1, 1e2, 1e3],
-    #                           "gamma": np.logspace(-2, 2, 5)})
-	model = SVR(C=1.0, epsilon=0.2, kernel ='rbf')
+	model = SVR()
+	#model = GridSearchCV(model, parameters)
+	model.fit(X_train, y_train)
+	print '{}'.format(model.score(X_test, y_test))
+	#model = model.best_estimator_
+	#print "Parameters are {} for the optimal model.".format(model.get_params())
+	plot_tunned_model('SVR', model, X, y)
 
-	predicted = cross_val_predict(model, X, y, cv=10)
-	scores = cross_val_score(model, X, y, cv=10)
-	print "Scores {}". format(scores)
-	print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
-
-	fig, ax = plt.subplots()
-	ax.scatter(y, predicted)
-	ax.plot([y.min(), y.max()], [y.min(), y.max()], 'k--', lw=4)
-	ax.set_xlabel('Measured')
-	ax.set_ylabel('Predicted')
-	plt.show()
-	#model = GridSearchCV(SVR(kernel='rbf', gamma=0.1), cv=5,
-    #               param_grid={"C": [1e0, 1e1, 1e2, 1e3],
-    
-    #                           "gamma": np.logspace(-2, 2, 5)})
+def lasso_model(X_train, X_test, y_train, y_test):
+	print "********************Lasso Model******************"
+	model = Lasso(random_state=42)
 	model.fit(X_train, y_train)
 	print '{}'.format(model.score(X_test, y_test))
 
-	plot_results(X_test, X_train, y_test, y_train, model, y)
+def lasso_model_tunned(X_train, X_test, y_train, y_test, X, y):
+	print "********************Lasso Model Tunned Model******************"
+	parameters = {'alpha': [0.1, 0.5, 1.0], 'max_iter':(1000,10000, 150000), 'fit_intercept': (True, False), 'selection': ('random', 'cyclic')}
+	model = Lasso(random_state=42)
+	model = GridSearchCV(model, parameters)
+	model = model.fit(X_train, y_train)
+	print '{}'.format(model.score(X_test, y_test))
+	plot_tunned_model('Lasso', model, X, y)
 
-def lasso_model(X, y):
-	X_train, X_test, y_train, y_test = split_data(X, y)
-	model = Lasso(alpha=0.1, max_iter=10000)
+
+def neighbors_model(X_train, X_test, y_train, y_test, X, y):
+	print "********************KNeighborsRegressor Model******************"
+
+	#parameters = {'weights':('uniform', 'distance'), 'n_neighbors':[2,3,5]}
+	model = KNeighborsRegressor()
+	#model = GridSearchCV(kn, parameters)
 	model.fit(X_train, y_train)
 	print '{}'.format(model.score(X_test, y_test))
-
-
-def neighbors_model(X, y):
-	X_train, X_test, y_train, y_test = split_data(X, y)
-	parameters = {'weights':('uniform', 'distance'), 'n_neighbors':[2,3,5]}
-	kn = KNeighborsRegressor()
-	model = GridSearchCV(kn, parameters)
-
-	predicted = cross_val_predict(model, X, y, cv=10)
-	scores = cross_val_score(model, X, y, cv=10)
-	print "Scores {}". format(scores)
-	print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
-
-	fig, ax = plt.subplots()
-	ax.scatter(y, predicted)
-	ax.plot([y.min(), y.max()], [y.min(), y.max()], 'k--', lw=4)
-	ax.set_xlabel('Measured')
-	ax.set_ylabel('Predicted')
-	plt.show()
-
-
-
-	model.fit(X_train, y_train) 
-	print '{}'.format(model.score(X_test, y_test))
-	#plot_results(X_test, X_train, y_test, y_train, model, y)
-
+	#model = model.best_estimator_
+	#print "Parameters are {} for the optimal model.".format(model.get_params())
+	plot_tunned_model('KNeighborsRegressor', model, X, y)
 
 
 def run():
 	#save_file('Google')
 	stock = main(sys.argv[1:])
 	X, y = read_from_file(stock)
+	X_train, X_test, y_train, y_test = split_data(X, y)
+	plot_split_results(X_train, X_test, y_train, y_test)
 
-	
-	#linear_model(X, y)
-	lasso_model(X, y)
+	benchmark(X_train, X_test, y_train, y_test)
+	#svr_model(X_train, X_test, y_train, y_test, X, y)
+	#neighbors_model(X_train, X_test, y_train, y_test, X, y)
+	#linear_model(X_train, X_test, y_train, y_test)
+	#lasso_model(X_train, X_test, y_train, y_test)
+	#linear_model_tunned(X_train, X_test, y_train, y_test, X, y)
+	lasso_model_tunned(X_train, X_test, y_train, y_test, X, y)
 
 
 
